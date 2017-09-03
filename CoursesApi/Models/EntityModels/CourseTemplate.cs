2e3cb2ec4f42fcd612c911f99a75ecadc424ff55
Hreiðar Ólafur Arnarsson <spekingurinn@gmail.com>
2017-09-03 22:31:47 +0000

    added templates for entities

---
+++ CoursesApi/Models/EntityModels/CourseTemplate.cs
@@ -0,0 +1,8 @@
+namespace CoursesApi.Models.EntityModels
+{
+	public class CourseTemplate
+	{
+		public string Name { get; set; }
+		public string CourseID { get; set; }
+	}
+}
---
+++ CoursesApi/Models/EntityModels/Student.cs
@@ -0,0 +1,8 @@
+namespace CoursesApi.Models.EntityModels
+{
+	public class Student
+	{
+		public int SSN { get; set; }
+		public string Name { get; set; }
+	}
+}
