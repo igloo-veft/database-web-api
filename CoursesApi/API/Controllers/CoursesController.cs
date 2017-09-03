96a0db2f5f0ee4af9cae5be0f515b64a07e57742
Hreiðar Ólafur Arnarsson <spekingurinn@gmail.com>
2017-09-03 22:51:05 +0000

    added possible actions to controller

--- CoursesApi/API/Controllers/CoursesController.cs
+++ CoursesApi/API/Controllers/CoursesController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using CoursesApi.Services;
+using CoursesApi.Models.EntityModels;
 
 namespace API.Controllers
 {
@@ -29,9 +30,9 @@ namespace API.Controllers
         // GET api/courses/1
         [HttpGet]
         [Route("{id}")]
-        public IActionResult GetCourse(int id)
+        public IActionResult GetCourseById(int id)
         {
-            var course = _coursesService.GetCourses();
+            var course = _coursesService.GetCourseById(id);
             return Ok(course);
         }
 
@@ -44,23 +45,34 @@ namespace API.Controllers
         // PUT api/courses/1
         [HttpPut]
         [Route("{id}")]
-        public void Put(int id, [FromBody]string value)
+        public IActionResult UpdateCourse(int id, [FromBody]Course course)
         {
+            return Ok();
         }
 
         // DELETE api/courses/1
         [HttpDelete]
         [Route("{id}")]
-        public void Delete(int id)
+        public IActionResult DeleteCourse(int id)
         {
+            return Ok();
         }
 
         // GET api/courses/1/students
         [HttpGet]
         [Route("{id}/students")]
+        public IActionResult GetStudentsInCourse(int id)
+        {
+            var studentlist = _coursesService.GetStudentsInCourse(id);
+            return Ok(studentlist);
+        }
 
         // POST api/courses/1/students
         [HttpPost]
         [Route("{id}/students")]
+        public IActionResult AddStudentToCourse(int id, [FromBody]Student student)
+        {
+            return Ok();
+        }
     }
 }
